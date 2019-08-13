67bc020526b7d1eeb39f8dde03fce3beb91d265d
Olivier ESPION <oespion@e1r2p12.42.fr>
2019-08-13 16:31:57 +0200

    start parsing data

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func epur_zero(result []string) []string {
+	result[0] = strings.Replace(result[0], "X^0", "1", -1)
+	result[1] = strings.Replace(result[1], "X^0", "1", -1)
+	return result
+}
 
 func main() {
-	fmt.Println("nique.jpg")
-	return;
+	args := strings.Replace(os.Args[1:][0], " ", "", -1)
+	// arg1 := args[:strings.IndexByte(args, '=')]
+	result := strings.Split(args, "=")
+	fmt.Println(result)
+	result = epur_zero(result)
+	fmt.Println(result)
+	return
 }
