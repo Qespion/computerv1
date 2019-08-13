f1bd2953736c1d6a37aea5e3d92099c17c53ffe0
Olivier ESPION <oespion@e1r2p12.42.fr>
2019-08-13 18:45:57 +0200

    reduced form

--- main.go
+++ main.go
@@ -14,16 +14,45 @@ func epur_zero(result []string) []string {
 	return result
 }
 
+func check_degree(result string) bool {
+	red := ansi.ColorFunc("red+")
+	for i, rune := range result {
+		if rune == 94 {
+			if result[i+1] > 50 {
+				fmt.Printf(red("▶ WARNING : The polynomial degree is stricly greater than 2, I can't solve! \n"))
+				return false
+			}
+			if i+2 < len(result) {
+				if result[i+2] >= 48 && result[i+2] <= 57 {
+					fmt.Printf(red("▶ WARNING : The polynomial degree is stricly greater than 2, I can't solve.! \n"))
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+func reduced_form(args string) {
+	result := strings.Split(args, "=")
+	result = epur_zero(result)
+	for len(result[1]) > 0 {
+		result[1] = result[1][1:]
+		fmt.Println(result[1])
+	}
+	return
+}
+
 func main() {
-	if len(os.Args[1:]) <= 1 {
+	if len(os.Args[1:]) < 1 {
 		red := ansi.ColorFunc("red+")
 		fmt.Printf(red("▶ WARNING : Please insert THINGS has argument! \n"))
 		return
 	}
 	args := strings.Replace(os.Args[1:][0], " ", "", -1)
-	result := strings.Split(args, "=")
-	fmt.Println(result)
-	result = epur_zero(result)
-	fmt.Println(result)
+	if check_degree(args) == false {
+		return
+	}
+	reduced_form(args)
 	return
 }
